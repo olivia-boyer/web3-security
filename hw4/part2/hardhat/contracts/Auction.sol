b2bb0bc5baf9f01f41603aef74e74a60ba23f34f
Olivia Boyer <oliviaboyer@wc116-dhcp-67.brynmawr.edu>
2026-03-15 13:15:39 -0400

    part 2

--- hw4/part2/hardhat/contracts/Auction.sol
+++ hw4/part2/hardhat/contracts/Auction.sol
@@ -48,32 +48,45 @@ contract Auction {
         _;
     }
 
-    function startAuction() public /* MODIFIER(S) */ {
+    error BidTooLow();
+    error SenderAlreadyBid();
+    error SenderHasNotYetBid();
+    error CantRefundWinner();
+
+    function startAuction() public onlyOwner() {
         /* 
             Start the auction by setting the startTime variable
             Permissions - only the owner should be allowed to start the auction.
          */
+        startTime = block.timestamp;
+        endTime = 0;
 
     }
 
-    function endAuction() public /* MODIFIER(S) */ {
+    function endAuction() public onlyOwner() isActive() {
         /* 
             End the auction by setting the startTime variable
             Permissions - only the owner should be allowed to end the auction.
          */
+        endTime = block.timestamp;
     }
 
-    function makeBid() public payable /* MODIFIER(S) */ {
+    function makeBid() public payable isActive() {
         /* 
             Only allow the bid to go through if it is higher than the current highest bid and the bidder has not yet bid.
             Set the highestBidder, and highestBid variables accordingly.
             
             Update the fundsPerBidder map.
          */
+        require(msg.value > highestBid, "bid too low");
+        require (fundsPerBidder[msg.sender] == 0, "sender already bid");
+        fundsPerBidder[msg.sender] = msg.value;
+        highestBidder = payable(msg.sender);
+        highestBid = msg.value;
 
     }
 
-    function upBid() public payable /* MODIFIERS(S) */ {
+    function upBid() public payable isActive() {
         /* 
             upBid will update the bidder's bid to their current bid + the msg.value being added.
             Only allow the upBid to go through if their new bid price is higher than the current bid and they have already bid. 
@@ -83,10 +96,21 @@ contract Auction {
             Update the fundsPerBidder map.
 
         */
+           if (fundsPerBidder[msg.sender] != 0) {
+            if (fundsPerBidder[msg.sender] + msg.value > highestBid) {
+            fundsPerBidder[msg.sender] += msg.value;
+            highestBidder = payable(msg.sender);
+            highestBid = fundsPerBidder[msg.sender];
+            } else {
+                revert BidTooLow();
+            }
+        } else {
+            revert SenderHasNotYetBid();
+        }
 
     }
 
-    function refund() public /* MODIFIER(S) */ {
+    function refund() public isClosed() {
         /* 
             For the refunds, the loser will individually call this function.
             Refunds won't be made to all losers in a batch. You will see in Part 3 why that is a bad design pattern.
@@ -100,10 +124,20 @@ contract Auction {
             Hint 1: You only need a reciever's public key to send them ETH. 
             Hint 2: Use the solidity transfer function to send the funds. 
         */
+       if (msg.sender != highestBidder) {
+        //copied transfer method from here: https://www.cyfrin.io/glossary/sending-ether-transfer-send-call-solidity-code-example
+        bool bidded = fundsPerBidder[msg.sender] > 0;
+        require(bidded, "Sender didn't bid!");
+        (bool sent, ) = payable(msg.sender).call{value: fundsPerBidder[msg.sender]}("");
+        require(sent, "Failed to send Ether");
+        fundsPerBidder[msg.sender] = 0;
+       } else {
+        revert CantRefundWinner();
+       }
 
     }
 
-    function payoutWinner() public /* MODIFIER(S) */ {
+    function payoutWinner() public onlyOwner() isClosed() {
         fundsPerBidder[highestBidder] = 0;
         nft.enterAddressIntoBook("auction");
         nft.mintNFT();
